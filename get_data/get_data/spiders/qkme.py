8854a3460ee6248e733457a381447e37a88af308
Jay Hack <jayhack.0@gmail.com>
2013-04-06 15:19:12 -0700

    changed all 'href' variable names to 'url'

--- get_data/get_data/spiders/qkme.py
+++ get_data/get_data/spiders/qkme.py
@@ -16,7 +16,7 @@ class QkmeSpider (BaseSpider):
     def __init__ (self):
 
         #list to contain the absolute links to each instance page
-        self.instance_page_hrefs = []
+        self.instance_page_urls = []
 
         pass
     
@@ -34,28 +34,28 @@ class QkmeSpider (BaseSpider):
         hxs = HtmlXPathSelector(response)
         
         #xpath expression to extract the links to each meme instance page
-        instance_page_hrefs_xpath = '//div[@class="memeThumb"]/a/@href'
+        instance_page_urls_xpath = '//div[@class="memeThumb"]/a/@href'
         
-        #xpath for the 'next page' href
-        next_page_hrefs_xpath = '//a[@class="next"]/@href'
+        #xpath for the 'next page' url
+        next_page_urls_xpath = '//a[@class="next"]/@href'
         
-        instance_page_hrefs = hxs.select(instance_page_hrefs_xpath).extract ()
-        next_page_hrefs = hxs.select (next_page_hrefs_xpath).extract ()
+        instance_page_urls = hxs.select(instance_page_urls_xpath).extract ()
+        next_page_urls = hxs.select (next_page_urls_xpath).extract ()
 
 
         BaseURL = 'http://www.quickmeme.com'
         
-        #Get the links to instance pages, store them in self.instance_page_hrefs
-        for link in instance_page_hrefs:
-            self.instance_page_hrefs.append(BaseURL + link)
+        #Get the links to instance pages, store them in self.instance_page_urls
+        for link in instance_page_urls:
+            self.instance_page_urls.append(BaseURL + link)
 
 
         requests = []
         #Get link to the next page of this meme-type
-        for link in next_page_hrefs:
-            next_page_href = BaseURL + link
-            requests.append (Request (next_page_href, callback=self.parse))
-            print next_page_href
+        for link in next_page_urls:
+            next_page_url = BaseURL + link
+            requests.append (Request (next_page_url, callback=self.parse))
+            print next_page_url
 
         #case - still more meme-type pages to go through.
         # return a request object for the next page
@@ -63,12 +63,12 @@ class QkmeSpider (BaseSpider):
             return requests
 
         #case - no more meme-type pages left
-        # return a list of requests for the meme instance pages from links stored in self.instance_page_hrefs
+        # return a list of requests for the meme instance pages from links stored in self.instance_page_urls
         else:
             self.print_status("Finished scanning meme-type pages")
-            for href in self.instance_page_hrefs:
-                print href
-                requests.append (Request (href, callback=self.parse_instance_page))
+            for url in self.instance_page_urls:
+                print url
+                requests.append (Request (url, callback=self.parse_instance_page))
             self.print_status("Beginning to scan meme-instance pages")
             return requests
 
