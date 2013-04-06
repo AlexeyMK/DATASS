b95eadd68cbbe9fc448c3a26ef495cffd2ea0769
Jay Hack <jayhack.0@gmail.com>
2013-04-06 13:40:38 -0700

    setup scrapy module 'get_data'

---
+++ get_data/get_data/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for get_data project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'get_data'
+
+SPIDER_MODULES = ['get_data.spiders']
+NEWSPIDER_MODULE = 'get_data.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'get_data (+http://www.yourdomain.com)'
