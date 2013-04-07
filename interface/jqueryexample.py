428498b4e50ce6c2cb8e70d66bf5bc86b99f6344
Jay Hack <jayhack.0@gmail.com>
2013-04-07 10:01:58 -0700

    about to put real html up

--- interface/jqueryexample.py
+++ interface/jqueryexample.py
@@ -8,42 +8,65 @@
     :copyright: (c) 2010 by Armin Ronacher.
     :license: BSD, see LICENSE for more details.
 """
-
 import sys
 import os
 sys.path.append (os.path.abspath ('../process_data/'))
 from SentimentAnalysis import SentimentAnalysis
-from flask import Flask, jsonify, render_template, request
-
 import pickle
+import itertools
+import json
+from operator import itemgetter
+from flask import Flask, request, render_template, jsonify
+
+image_locations = {
+    'Annoying Facebook Girl': 'http://i.imgflip.com/1bhi.jpg',
+    'First World Problems': 'http://i.imgflip.com/1bhf.jpg',
+    'Forever Alone': 'http://i.imgflip.com/1bh4.jpg',
+    'Foul Bachelor Frog': 'http://i.imgflip.com/1bgv.jpg',
+    'Futurama Fry': 'http://i.imgflip.com/1bgw.jpg',
+    'Good Guy Greg': 'http://i.imgflip.com/1bgx.jpg',
+    'Insanity Wolf': 'http://i.imgflip.com/1bgu.jpg',
+    'Paranoid Parrot': 'http://i.imgflip.com/1bi4.jpg',
+    'Philosoraptor': 'http://i.imgflip.com/1bgs.jpg',
+    'Scumbag Steve': 'http://i.imgflip.com/1bgy.jpg',
+    'Socially Awkward Penguin': 'http://i.imgflip.com/1bh0.jpg',
+    'Success Kid': 'http://i.imgflip.com/1bhk.jpg',
+    'The Most Interesting Man In The World': 'http://i.imgflip.com/1bh8.jpg'
+    }
+
+
+sa = SentimentAnalysis ('../process_data/Trained_Classifier.obj')
+
 app = Flask(__name__)
-
-
-@app.route('/_add_numbers')
-def add_numbers():
-    """Add two numbers server side, ridiculous but well..."""
-    a = request.args.get('a', 0, type=int)
-    b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b)
-    
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/_classify_text')
-def classify_text ():
-    sentence = request.args.get('sentence', 0, type=str)
-    print sentence
-    pass
+@app.route("/_classify_sentiment")
+def classify_sentiment ():
+    sentence = request.args.get('a', 0, type=str)
     
+    prob_dist = sa.maxent_classify_raw (sentence)
     
+    probabilities = {}
+    for type in prob_dist.samples ():
+        probabilities[type] = prob_dist.prob(type)
+    
+    rankings = []
+    for key, value in probabilities.iteritems ():
+        rankings.append ((key, value))
+    sorted_rankings = sorted(rankings, key=itemgetter(1), reverse=True)
 
+    r = [s[0] + (" - %f" % s[1]) for s in sorted_rankings]
+    urls = [[s[0], image_locations[s[0]]] for s in sorted_rankings]
+
+    packed_rankings = json.dumps (urls)
+
+    return jsonify(result=packed_rankings)
 
 if __name__ == '__main__':
-    f = open ('../process_data/Trained_Classifier.obj', 'r')
-    classifier = pickle.load(f)
-    
-    
+
+    app.debug = True
     app.run()
