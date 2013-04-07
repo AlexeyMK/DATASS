fb62d78c42ff9fe12d5064e974415333809d0c24
Jay Hack <jayhack.0@gmail.com>
2013-04-06 23:21:32 -0700

    cosine similarity works here, though not very well. this is a naive implementation without any sort of weighting for the different sentiments. we need to also integrate unigrams, bigrams, etc

--- process_data/SentimentAnalysis.py
+++ process_data/SentimentAnalysis.py
@@ -2,7 +2,9 @@
 import csv
 from nltk.tokenize import word_tokenize, wordpunct_tokenize, sent_tokenize
 from collections import defaultdict
-import pprint
+import operator
+import math
+
 
 
 class SentimentAnalysis:
@@ -21,16 +23,27 @@ class SentimentAnalysis:
             "../data/Scumbag-Steve data.txt",
             "../data/Socially-Awkward-Penguin data.txt",
             "../data/Success-Kid data.txt",
-            "../data/Paranoid-Parrot data.txt"
+            "../data/Paranoid-Parrot data.txt",
+            "../data/Annoying-Facebook-Girl data.txt",
+            "../data/First-World-Problems data.txt",
+            "../data/Forever-Alone data.txt",
+            "../data/The-Most-Interesting-Man-In-The-World data.txt"
         ]
-            
+        
+        data_filenames_test = [
+            "../data/test_data/data.txt",
+            "../data/test_data/data2.txt"
+        ]
     
     
         #dict mapping words to the sentiments they contain
         self.word_sentiments = defaultdict(lambda: set())
         
         #dict mapping meme-types to their sentiment vectors (normalized)
+        
         self.sentiment_vectors = defaultdict(lambda: defaultdict(lambda: 0.0))
+        self.sentiment_vectors_top = defaultdict(lambda: defaultdict(lambda: 0.0))
+        self.sentiment_vectors_bottom = defaultdict(lambda: defaultdict(lambda: 0.0))
         
         # Get the sentiment vectors for each meme
         self.get_word_sentiments('../data/inquirerbasic.csv')
@@ -38,15 +51,42 @@ class SentimentAnalysis:
             self.calculate_memetype_sentiment_vector (filename)
         
         
-        #sv = self.calculate_sentence_sentiment_vector(['I', 'Went', 'to', 'the', 'store', 'and', 'saw', 'a', 'cute', 'girl', 'but', 'i', 'was', 'too', 'shy', 'too', 'even', 'say', 'hi'])
-        sv = self.calculate_sentence_sentiment_vector(['awkward', 'shy', 'help', 'sad', 'fuck', 'awkward', 'awkward'])
-
-    
-        cs = []
+        #sv = self.calculate_sentence_sentiment_vector(['I', 'Went', 'to', 'the', 'store', 'and', 'saw', 'a', 'cute', 'girl', 'but', 'i', 'was', 'too', 'shy', 'too', 'even', 'say', 'hi']) #SAP
+        #sv = self.calculate_sentence_sentiment_vector(['awkward', 'shy', 'help', 'sad', 'fuck', 'awkward', 'awkward']) #SAP
+        #sv = self.calculate_sentence_sentiment_vector(['my', 'girlfriend', 'gave', 'me', 'mouth', 'sex', 'and', 'it', 'was', 'awesome', 'because', 'she', 'has', 'huge', 'breasts']) #Success Kid
+        sv = self.calculate_sentence_sentiment_vector(['I', 'won', 'cash', 'success'])
+        #sv = self.calculate_sentence_sentiment_vector(['happy'])
+        matches_all = {}
+        matches_top = {}
+        matches_bottom = {}
+        
         for v in self.sentiment_vectors:
             cosine = self.calculate_cosine_similarity (self.sentiment_vectors[v], sv)
-            print v, " : ", cosine
+            matches_all[v] = cosine
+        sorted_all = sorted(matches_all.iteritems(), key=operator.itemgetter(1))
         
+        for v in self.sentiment_vectors_top:
+            cosine = self.calculate_cosine_similarity (self.sentiment_vectors_top[v], sv)
+            matches_top[v] = cosine
+        sorted_top = sorted(matches_top.iteritems(), key=operator.itemgetter(1))
+        
+        for v in self.sentiment_vectors_bottom:
+            cosine = self.calculate_cosine_similarity (self.sentiment_vectors_bottom[v], sv)
+            matches_bottom[v] = cosine
+        sorted_bottom = sorted(matches_bottom.iteritems(), key=operator.itemgetter(1))
+            
+        
+        print "#### TOP ALL:"
+        for v in sorted_all:
+            print v[0], ": ", v[1]
+        
+        print "#### TOP TOP:"
+        for v in sorted_top:
+            print v[0], ": ", v[1]
+            
+        print "#### TOP BOTTOM:"
+        for v in sorted_bottom:
+            print v[0], ": ", v[1]
         
         pass
 
@@ -83,6 +123,7 @@ class SentimentAnalysis:
         for key, value in sentiment_vector.iteritems():
             squared_total += value*value
     
+        squared_total = math.sqrt(squared_total)
         for key,value in sentiment_vector.iteritems():
             sentiment_vector[key] = value / squared_total
 
@@ -105,6 +146,7 @@ class SentimentAnalysis:
     def calculate_memetype_sentiment_vector (self, filename):
         f = open(filename, 'r')
         
+        meme_type = ''
         contents = f.readlines()
         for entry in contents:
             
@@ -115,14 +157,19 @@ class SentimentAnalysis:
 
             for word in wordpunct_tokenize(top_text):
                 self.add_word_occurence_to_sentiment_vector (word, self.sentiment_vectors[meme_type])
+                self.add_word_occurence_to_sentiment_vector (word, self.sentiment_vectors_top[meme_type])
 
             for word in wordpunct_tokenize(bottom_text):
                 self.add_word_occurence_to_sentiment_vector (word, self.sentiment_vectors[meme_type])
-    
+                self.add_word_occurence_to_sentiment_vector (word, self.sentiment_vectors_bottom[meme_type])
+                    
         self.normalize_sentiment_vector (self.sentiment_vectors[meme_type])
-        print "\n\n###########", meme_type, ": #############\n", self.sentiment_vectors[meme_type]
+        self.normalize_sentiment_vector (self.sentiment_vectors_top[meme_type])
+        self.normalize_sentiment_vector (self.sentiment_vectors_bottom[meme_type])
+        #print "\n\n###########", meme_type, ": #############\n", self.sentiment_vectors[meme_type]
             
-
+            
+    
     # Function: calculate_sentence_sentiment_vectors
     # ----------------------------------------------
     # given a list of words that make up a sentence, this function will return a sentiment vector
@@ -132,10 +179,9 @@ class SentimentAnalysis:
         for word in sentence:
             self.add_word_occurence_to_sentiment_vector (word.lower(), sentiment_vector)
         self.normalize_sentiment_vector (sentiment_vector)
-        print "############## sentence vector: ###############\n"
+        #print "############## sentence vector: ###############\n"
         print sentiment_vector
         return sentiment_vector
-
     
 
     # Function: calculate_cosine_similarity
