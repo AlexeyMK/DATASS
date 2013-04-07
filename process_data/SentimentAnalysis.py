d42c6bf4f86cb06ff52214bd5bdbe91204c84f2b
Jay Hack <jayhack.0@gmail.com>
2013-04-06 21:57:27 -0700

    added simple cosine similarity

--- process_data/SentimentAnalysis.py
+++ process_data/SentimentAnalysis.py
@@ -35,10 +35,24 @@ class SentimentAnalysis:
         # Get the sentiment vectors for each meme
         self.get_word_sentiments('../data/inquirerbasic.csv')
         for filename in data_filenames:
-            self.calculate_sentiment_vector (filename)
+            self.calculate_memetype_sentiment_vector (filename)
+        
+        
+        sv = self.calculate_sentence_sentiment_vector(['I', 'Went', 'to', 'the', 'store', 'and', 'saw', 'a', 'cute', 'girl', 'but', 'i', 'was', 'too', 'shy', 'too', 'even', 'say', 'hi'])
+        
+        cs = []
+        for v in self.sentiment_vectors:
+            cosine = self.calculate_cosine_similarity (self.sentiment_vectors[v], sv)
+            print v, " : ", cosine
+        
         
         pass
 
+
+
+    ###########################################################################################################################
+    ############################################[ GETTING SENTIMENT VECTORS ]##################################################
+    ###########################################################################################################################    
 
     # Function: get_word_sentiments
     # -----------------------------
@@ -59,7 +73,6 @@ class SentimentAnalysis:
             i += 1
 
 
-
     # Function: normalize_sentiment_vector
     # ------------------------------------
     # makes sure the sentiment vector has a length of 1
@@ -71,27 +84,23 @@ class SentimentAnalysis:
         for key,value in sentiment_vector.iteritems():
             sentiment_vector[key] = value / squared_total
 
-    
-    
-
 
     # Function: add_word_occurence_to_sentiment_vector
     # ------------------------------------------------
     # given a word and the meme-type under which it occurs, this function will add it's represented
     # sentiments to the meme-type's sentiment vector. discards the word if it isn't in our sentiment corpus
-    def add_word_occurence_to_sentiment_vector (self, word, meme_type):
+    def add_word_occurence_to_sentiment_vector (self, word, sentiment_vector):
         
         word = word.lower()
         this_word_sentiments = self.word_sentiments[word]
         for sentiment in this_word_sentiments:
-            self.sentiment_vectors[meme_type][sentiment] += 1
+            sentiment_vector[sentiment] += 1
         
 
-
-    # Function: calculate_sentiment_vector
+    # Function: calculate_memetype_sentiment_vector
     # ------------------------------------
     # enter in the filename of a meme, it will calculate the sentiment vector for it.
-    def calculate_sentiment_vector (self, filename):
+    def calculate_memetype_sentiment_vector (self, filename):
         f = open(filename, 'r')
         
         contents = f.readlines()
@@ -103,16 +112,39 @@ class SentimentAnalysis:
             bottom_text = fields[2]
 
             for word in wordpunct_tokenize(top_text):
-                self.add_word_occurence_to_sentiment_vector (word, meme_type)
+                self.add_word_occurence_to_sentiment_vector (word, self.sentiment_vectors[meme_type])
 
             for word in wordpunct_tokenize(bottom_text):
-                self.add_word_occurence_to_sentiment_vector (word, meme_type)
-
+                self.add_word_occurence_to_sentiment_vector (word, self.sentiment_vectors[meme_type])
+    
         self.normalize_sentiment_vector (self.sentiment_vectors[meme_type])
-
-        print "###########", meme_type, ": #############\n" self.sentiment_vectors[meme_type])
+        print "\n\n###########", meme_type, ": #############\n", self.sentiment_vectors[meme_type]
             
 
+    # Function: calculate_sentence_sentiment_vectors
+    # ----------------------------------------------
+    # given a list of words that make up a sentence, this function will return a sentiment vector
+    def calculate_sentence_sentiment_vector (self, sentence):
+
+        sentiment_vector = defaultdict(lambda: 0.0)
+        for word in sentence:
+            self.add_word_occurence_to_sentiment_vector (word.lower(), sentiment_vector)
+        self.normalize_sentiment_vector (sentiment_vector)
+        print "############## sentence vector: ###############\n"
+        print sentiment_vector
+        return sentiment_vector
+
+    
+
+    # Function: calculate_cosine_similarity
+    # -------------------------------------
+    # basically take the dot-product between two vectors in order to find the theta between them
+    def calculate_cosine_similarity (self, sentence_vector, meme_type_vector):
+
+        sum = 0.0
+        for key, value in sentence_vector.iteritems ():
+            sum += sentence_vector[key] * meme_type_vector[key]
+        return sum
             
         
 
